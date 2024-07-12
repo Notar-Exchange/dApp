0e544eab406b82dbed6b8919f57660261bc55daf
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-12 22:04:58 +0200

    feat(app): shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="bg-background flex min-h-screen flex-col items-center justify-center text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Notar<span className="text-[hsl(280,100%,70%)]">.network</span>
+        <h1 className="text-5xl font-extrabold tracking-tight text-gray-500 sm:text-[5rem]">
+          Notar<span className="text-foreground">.network</span>
         </h1>
       </div>
     </main>
