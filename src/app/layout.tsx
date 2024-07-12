78b4b4ba6470d82e805ed8fd80def109cf918668
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-12 23:02:26 +0200

    feat(next): disable caching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import "@/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+// - Route Segment Config
+export const dynamic = "force-dynamic";
+
+export const revalidate = 0;
+
 export const metadata: Metadata = {
   title: "Notar Network",
   description: "A decentralized notary network",
