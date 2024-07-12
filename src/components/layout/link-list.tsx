eaa2644f1ef25c515916d52d7a1302915b074867
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-12 23:59:51 +0200

    feat(comp): link-list

---
+++ src/components/layout/link-list.tsx
@@ -0,0 +1,17 @@
+/*
+ * File: /src/components/layout/link-list.tsx
+ * Project: notar
+ * Created: Friday, 12th July 2024
+ * Author: @rashadatjou
+ * -----
+ * Copyright 2024, ©Notar
+ * -----
+ */
+
+function LinkList() {
+  return <div>Link</div>;
+}
+
+LinkList.displayName = "LinkList";
+
+export default LinkList;
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -10,23 +10,26 @@
 
 import Logo from "@/components/icon/Logo";
 import Link from "next/link";
+import LinkList from "@/components/layout/link-list";
 
 import { Routes } from "@/server/routes";
 
 function Navbar() {
   return (
-    <div className="fixed start-0 top-0 z-50 h-14 w-full border-b-2 bg-muted p-4">
+    <nav className="fixed start-0 top-0 z-50 h-14 w-full border-b-2 bg-muted p-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
           <Link href={Routes.path.default}>
             <Logo />
           </Link>
+
+          <LinkList />
         </div>
         <div>
           <button className="btn btn-ghost btn-circle">Theme</button>
         </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
