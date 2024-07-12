2b965a389ee1f9620c843084d2c40337130d4441
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 00:39:05 +0200

    feat(layout): mobile sidebar

--- src/components/layout/m.sidebar.tsx
+++ src/components/layout/m.sidebar.tsx
@@ -8,10 +8,44 @@
  * -----
  */
 
+"use client";
+
 import type { HTMLAttributes } from "react";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
+import { Routes } from "@/server/routes";
+import Link from "next/link";
+import Logo from "@/components/icon/Logo";
+import LinkList from "@/components/layout/link-list";
 
 function MobileSidebar(props: HTMLAttributes<HTMLDivElement>) {
-  return <div {...props}>Sidebar</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div {...props}>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <MenuIcon />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <SheetHeader className="pb-4">
+            <Link href={Routes.path.default} onClick={() => setOpen(false)}>
+              <Logo />
+            </Link>
+          </SheetHeader>
+          <div className="space-y-1">
+            <LinkList variant="horizontal" onClick={() => setOpen(false)} />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
 }
 
 MobileSidebar.displayName = "MobileSidebar";
