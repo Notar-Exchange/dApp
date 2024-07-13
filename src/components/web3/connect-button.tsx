9a74f046426c5e19ada1ad9d587efe16ad2a14d6
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 17:23:53 +0200

    feat(web3): connect-button uses new web3

--- src/components/web3/connect-button.tsx
+++ src/components/web3/connect-button.tsx
@@ -8,12 +8,32 @@
  * -----
  */
 
-import { Button } from "@/components/ui/button";
+"use client";
 
-function ConnectButton() {
+import { Button, type ButtonProps } from "@/components/ui/button";
+import { useWeb3, type UseWeb3AuthData } from "@/web3/hook/use-web3";
+
+type Props = ButtonProps;
+
+const loadTitle = (web3: UseWeb3AuthData): string => {
+  if (!web3.isConnected) {
+    return "Connect Wallet";
+  }
+
+  const address = web3.address;
+  if (!address) {
+    return "Re-Connect Wallet";
+  }
+
+  return address.slice(0, 8);
+};
+
+function ConnectButton(props: Props) {
+  const web3 = useWeb3();
+
   return (
-    <Button shape="pill" type="button">
-      Connect Wallet
+    <Button shape="pill" type="button" {...props} onClick={web3.login}>
+      {loadTitle(web3)}
     </Button>
   );
 }
