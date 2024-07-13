3a1f3423265381c7829789f9d56478fb37177c39
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 22:41:53 +0200

    feat(shadcn): card

---
+++ src/components/swap/card.tsx
@@ -0,0 +1,20 @@
+/*
+ * File: /src/components/swap/card.tsx
+ * Project: notar
+ * Created: Saturday, 13th July 2024
+ * Author: @rashadatjou
+ * -----
+ * Copyright 2024, ©Notar
+ * -----
+ */
+
+
+function SwapCard() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+
+    </div>
+  );
+}
+
+export default SwapCard;
