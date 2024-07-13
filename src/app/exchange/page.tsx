eb4d2df7e374d633c217cb7b32b85c6d8d4ae771
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 21:10:33 +0200

    feat: add exchange

---
+++ src/app/exchange/page.tsx
@@ -0,0 +1,21 @@
+/*
+ * File: /src/app/exchange/page.tsx
+ * Project: notar
+ * Created: Saturday, 13th July 2024
+ * Author: @rashadatjou
+ * -----
+ * Copyright 2024, ©Notar
+ * -----
+ */
+
+export default function ExchangePage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-gray-500 sm:text-[5rem]">
+          Exchange
+        </h1>
+      </div>
+    </div>
+  );
+}
