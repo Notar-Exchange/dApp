e18a03a49f20f6a167120730f1cfb24a480b9dd4
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 20:24:01 +0200

    refactor(web3auth): creating web3 auth

--- src/web3/web3auth/index.ts
+++ src/web3/web3auth/index.ts
@@ -13,30 +13,20 @@
 import { clientId, privateKeyProvider } from "./config";
 import { Web3AuthNoModal } from "@web3auth/no-modal";
 import { WEB3AUTH_NETWORK } from "@web3auth/base";
-import { env } from "@/env";
 import { MetamaskAdapter } from "@web3auth/metamask-adapter";
 
-const createWeb3Auth = () =>
-  new Web3AuthNoModal({
+export type Web3Auth = ReturnType<typeof createWeb3Auth>;
+
+export const createWeb3Auth = () => {
+  const web3auth = new Web3AuthNoModal({
     clientId,
     web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
     privateKeyProvider,
     enableLogging: true,
   });
 
-const globalForWeb3Auth = window as unknown as {
-  web3auth: ReturnType<typeof createWeb3Auth> | undefined;
-};
-
-const web3auth = globalForWeb3Auth.web3auth ?? createWeb3Auth();
-
-if (env.NEXT_PUBLIC_ENV !== "production") globalForWeb3Auth.web3auth = web3auth;
-
-if (!web3auth.connectedAdapterName) {
   const adapter = new MetamaskAdapter();
   web3auth.configureAdapter(adapter);
-}
 
-// - Public
-
-export { web3auth };
+  return web3auth;
+};
