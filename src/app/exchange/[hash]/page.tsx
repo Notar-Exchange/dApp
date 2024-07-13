824943c57f85edf67f6cd9d5ffa885d84781bc41
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 21:12:50 +0200

    feat(exchange): exchange page

--- src/app/exchange/[hash]/page.tsx
+++ src/app/exchange/[hash]/page.tsx
@@ -8,12 +8,12 @@
  * -----
  */
 
-export default function ExchangePage() {
+export default function ExchangePage({ params }: { params: { hash: string } }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-gray-500 sm:text-[5rem]">
-          Exchange
+          Exchange {params.hash}
         </h1>
       </div>
     </div>
