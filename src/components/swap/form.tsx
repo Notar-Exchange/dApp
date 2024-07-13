848be5e08928dde0937e3fb04b6125ed29be7769
Rashadatjou <rashadatjou.mehdi@gmail.com>
2024-07-13 23:00:41 +0200

    feat(swap): basic form setup

--- src/components/swap/form.tsx
+++ src/components/swap/form.tsx
@@ -10,8 +10,54 @@
 
 "use client";
 
+import { SwapSchema, type SwapSchemaType } from "@/schema/swap";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftRightIcon } from "lucide-react";
+
+// - Const
+const defaultValues: SwapSchemaType = {
+  amount: 1,
+};
+
+// - Component
 function SwapForm() {
-  return <div>Swap Form</div>;
+  const form = useForm<SwapSchemaType>({
+    defaultValues,
+    resolver: zodResolver(SwapSchema),
+  });
+
+  // - Actions
+  const onSubmit = async (values: SwapSchemaType) => {
+    console.log(values);
+  };
+
+  const onSwitch = () => {
+    console.log("Switch");
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-4">
+        <Button
+          className="absolute right-4 top-4"
+          variant="secondary"
+          type="button"
+          onClick={onSwitch}
+          disabled={form.formState.isSubmitting}
+        >
+          <ArrowLeftRightIcon />
+        </Button>
+
+        <div className="flex flex-col gap-y-4">
+          <div>input</div>
+          <div>input</div>
+        </div>
+      </form>
+    </Form>
+  );
 }
 
 SwapForm.displayName = "SwapForm";
---
+++ src/schema/swap.ts
@@ -0,0 +1,31 @@
+/*
+ * File: /src/schema/swap.ts
+ * Project: notar
+ * Created: Saturday, 13th July 2024
+ * Author: @rashadatjou
+ * -----
+ * Copyright 2024, ©Notar
+ * -----
+ */
+
+import { z } from "zod";
+
+export const SwapSchema = z.object({
+  amount: z.union([
+    z.coerce
+      .number({
+        message: "Amount must be a number",
+      })
+      .positive({
+        message: "Enter a valid number",
+      })
+      .min(1, {
+        message: "Minimum amount should be 1.0",
+      }),
+    z.literal("").refine(() => false, {
+      message: "Value is required",
+    }),
+  ]),
+});
+
+export type SwapSchemaType = z.infer<typeof SwapSchema>;
